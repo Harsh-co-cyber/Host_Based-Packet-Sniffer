b94a33c6cc7b89eacf7f179f54f1cc0600bbcc6f
Harsh Pahariya <harshpahariya19@gmail.com>
2025-11-03 23:31:29 +0530

    did some changes

--- app.py
+++ app.py
@@ -321,5 +321,7 @@ def capture_packets():
     })
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=False)
+
+
 
